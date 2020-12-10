6380e3365e53a7edaa90065306197e152f9d8c7f
theshreyas <74211645+theshreyas@users.noreply.github.com>
2020-12-11 03:38:35 +0530

    Create registration.php

---
+++ FormatIncrementId/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Theshreyas_FormatIncrementId',
+    __DIR__
+);
