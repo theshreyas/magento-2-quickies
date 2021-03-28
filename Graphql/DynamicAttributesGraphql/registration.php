48bddc35fc051a0351d31c23aa033d075636292b
theshreyas <74211645+theshreyas@users.noreply.github.com>
2021-03-28 13:08:39 +0530

    Delete Graphql/DynamicAttributesGraphql directory

--- Graphql/DynamicAttributesGraphql/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Theshreyas_DynamicAttributesGraphql',
-    __DIR__
-);
