51ebf9e2b2e5191004c800be5efac1f7e7c1f24c
theshreyas <74211645+theshreyas@users.noreply.github.com>
2021-03-28 13:07:47 +0530

    Delete GraphQl/Theshreyas/Amazonpay directory

--- GraphQl/Theshreyas/Amazonpay/README.md
+++
@@ -1,3 +0,0 @@
-# Theshreyas Amazonpay GraphQl Module
-
-Tested on 2.3.5-p2
--- GraphQl/Theshreyas/Amazonpay/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright ©  All rights reserved.
- * See COPYING.txt for license details.
- */
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Theshreyas_Amazonpay', __DIR__);
-
